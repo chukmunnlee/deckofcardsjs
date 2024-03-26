bd0fb8d7f3135831c948e17f15d5bee9b5b5ce7c
Chuk <chukmunnlee@gmail.com>
2024-03-26 15:12:13 +0800

    commandline options

---
+++ README.md
@@ -0,0 +1,7 @@
+# Deck of Cards API
+
+A deck of cards API inspired by [Deck of Cards API](https://www.deckofcardsapi.com/). The server is written in Golang using [Gin Gonic](https://github.com/gin-gonic/gin) web framework.
+
+Check out a deployed version at [deckofcards.chuklee.com](https://deckofcards.chuklee.com).
+
+If you are using this project to make something cool, let me know!
--- deckofcards/src/main.ts
+++ deckofcards/src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
+import {NestExpressApplication} from '@nestjs/platform-express';
+
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers'
+
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function bootstrap(argv: any) {
+
+  const PORT = argv.port
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  if (argv.cors)
+    app.enableCors()
+
+  app.listen(PORT, () => {
+    console.info(`Application started on port ${PORT} at ${new Date()}`)
+    if (argv.cors)
+      console.info('\tCORS enabled')
+  });
 }
-bootstrap();
+
+const argv = yargs(hideBin(process.argv))
+		.usage('Usage: $0 --cors --port [num] --games [num]')
+		.boolean(['cors'])
+		.default('port', 3000)
+		.default('cors', true)
+		.default('games', 10)
+		.parse()
+
+bootstrap(argv);
